2a3d3ca2fbc55d15c7e7a9c1f5ae1b79b21eed3e
grian0504 <107021035@gm.asia.edu.tw>
2019-10-21 02:01:40 +0800

    qq

---
+++ qq/whocares.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ * whocares
+ */
+public class whocares {
+
+    public static void main(String[] args) {
+        JFrame frame=new JFrame("First");
+        frame.setSize(400, 500);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
